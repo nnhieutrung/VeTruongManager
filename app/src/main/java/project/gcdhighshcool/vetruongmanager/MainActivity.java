42d077a13eaf4384877d61d4c73aca38e6e2aef0
nnhieutrung <67868869+nnhieutrung@users.noreply.github.com>
2020-10-25 21:05:38 +0700

    deloy

--- app/src/main/java/project/gcdhighshcool/vetruongmanager/MainActivity.java
+++ app/src/main/java/project/gcdhighshcool/vetruongmanager/MainActivity.java
@@ -1,12 +1,17 @@
 package project.gcdhighshcool.vetruongmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity
 {
-
+    private static String DB_URL = "34.92.244.48";
+    private static String USER_NAME = "vetruongadmin";
+    private static String PASSWORD = "vetruong123";
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
